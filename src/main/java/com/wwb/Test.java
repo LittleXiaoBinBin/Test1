6f9a99f360795ed8a08b6253e164a3596ed6c172
HeLianYun <1255897236@qq.com>
2018-09-23 16:46:26 +0800

    新建

---
+++ src/main/java/com/wwb/Test.java
@@ -0,0 +1,8 @@
+package com.wwb;
+
+public class Test {
+    public static void main(String[] args) {
+        Person tom = new Person("tom", 21);
+        System.out.println(tom.toString());
+    }
+}
