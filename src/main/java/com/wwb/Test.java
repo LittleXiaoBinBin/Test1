9d337c8561521bc13eb6efd0a17c6bf1a40672be
HeLianYun <1255897236@qq.com>
2018-09-23 17:36:29 +0800

    修改第二次

--- src/main/java/com/wwb/Test.java
+++ src/main/java/com/wwb/Test.java
@@ -3,9 +3,6 @@ package com.wwb;
 public class Test {
     public static void main(String[] args) {
         Person tom = new Person("tom", 21);
-        Person tom1 = new Person("tom", 21);
-        Person tom2 = new Person("tom", 21);
-        Person tom3 = new Person("tom", 21);
         System.out.println(tom.toString());
     }
 }
