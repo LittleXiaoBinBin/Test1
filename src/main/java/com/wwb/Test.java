2ff35f50b840667c4ec56cc22f73e243bcc9e081
HeLianYun <1255897236@qq.com>
2018-09-23 17:45:32 +0800

    san

--- src/main/java/com/wwb/Test.java
+++ src/main/java/com/wwb/Test.java
@@ -3,6 +3,7 @@ package com.wwb;
 public class Test {
     public static void main(String[] args) {
         Person tom = new Person("tom", 21);
+        Person tom1 = new Person("jack", 18);
         System.out.println(tom.toString());
     }
 }
