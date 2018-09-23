e0ae55a4b0e7d1b6d0c1f6655b289ebbe9552cff
HeLianYun <1255897236@qq.com>
2018-09-23 17:02:35 +0800

    新建分支

--- src/main/java/com/wwb/Test.java
+++ src/main/java/com/wwb/Test.java
@@ -3,6 +3,7 @@ package com.wwb;
 public class Test {
     public static void main(String[] args) {
         Person tom = new Person("tom", 21);
+        Person tom1 = new Person("tom", 21);
         System.out.println(tom.toString());
     }
 }
