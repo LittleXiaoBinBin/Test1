84e4e2bba3c075c3ab44191354fde01fdfc3cb3e
HeLianYun <1255897236@qq.com>
2018-09-23 17:26:13 +0800

    修改1

--- src/main/java/com/wwb/Test.java
+++ src/main/java/com/wwb/Test.java
@@ -4,6 +4,8 @@ public class Test {
     public static void main(String[] args) {
         Person tom = new Person("tom", 21);
         Person tom1 = new Person("tom", 21);
+        Person tom2 = new Person("tom", 21);
+        Person tom3 = new Person("tom", 21);
         System.out.println(tom.toString());
     }
 }
